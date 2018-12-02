b26f86599fdce4c134aee584d067d1fc1110f0ad
nkrim62 <nkrim62@gmail.com>
2018-12-01 19:36:52 -0800

    Initial commit - worked up to Random Child Generation

---
+++ Assets/My Assets/Scripts/Face.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Face : MonoBehaviour {
+
+    // Feature list
+    public enum Feature {
+        Head=0, Skin, Ears, Eyes, Nose
+    }
+
+    /* VARIABLES AND PROPERTIES */
+    // References to sprite renderers
+    SpriteRenderer head_sr;
+    SpriteRenderer ear_l_sr;
+    SpriteRenderer ear_r_sr;
+    SpriteRenderer eye_l_sr;
+    SpriteRenderer eye_r_sr;
+    SpriteRenderer nose_sr;
+
+    // Feature name paths
+    public static readonly string EarLeftName = "ear-left";
+    public static readonly string EarRightName = "ear-right";
+    public static readonly string EyeLeftName = "eye-left";
+    public static readonly string EyeRightName = "eye-right";
+    public static readonly string NoseName = "nose";
+
+    // Indices of features
+    protected List<int> indices;
+
+
+    /* LIFECYCLE METHODS */
+    // Use this for initialization
+    void Awake () {
+        // Init indices
+        indices = new List<int>();
+        int length = NumFeatures();
+        for(int i=0; i<length; i++) {
+            indices.Add(-1);
+        }
+    }
+	
+	
+    /* MAIN MODIFYING FUNCTIONS */
+    public void SetHead (GameObject head_prefab, int index) {
+        if (index >= 0)
+           indices[(int)Feature.Head] = index;
+        GameObject head = Instantiate(head_prefab);
+        head.transform.parent = transform;
+        // If a head already exists, prepare for swap
+        if (head_sr != null) {
+            // Get current properties
+            Color skin = head_sr.color;
+            Sprite ear = ear_l_sr.sprite;
+            Sprite eye = eye_l_sr.sprite;
+            Sprite nose = nose_sr.sprite;
+            // Destroy old head and reset references
+            DestroyImmediate(head_sr.gameObject);
+            FindSpriteRendererReferences();
+            // Reset values
+            SetSkin(skin, -1);
+            SetEars(ear, -1);
+            SetEyes(eye, -1);
+            SetNose(nose, -1);
+        }
+        else
+            FindSpriteRendererReferences();
+    }
+    public void SetSkin (Color skin, int index) {
+        if(index >= 0)
+            indices[(int)Feature.Skin] = index;
+        head_sr.color = skin;
+        ear_l_sr.color = skin;
+        ear_r_sr.color = skin;
+    }
+    public void SetEars (Sprite ear, int index) {
+        if (index >= 0)
+            indices[(int)Feature.Ears] = index;
+        ear_l_sr.sprite = ear;
+        ear_r_sr.sprite = ear;
+    }
+    public void SetEyes (Sprite eye, int index) {
+        if (index >= 0)
+            indices[(int)Feature.Eyes] = index;
+        eye_l_sr.sprite = eye;
+        eye_r_sr.sprite = eye;
+    }
+    public void SetNose (Sprite nose, int index) {
+        if (index >= 0)
+            indices[(int)Feature.Nose] = index;
+        nose_sr.sprite = nose;
+    }
+
+
+    /* MAIN INDEX ACCESSOR FUNCTIONS */
+    public List<int> GetIndices() {
+        return indices;
+    }
+    public int GetIndexOf(Feature f) {
+        return indices[(int)f];
+    }
+    public void DebugIndices() {
+        string res = "{";
+        foreach (int i in indices)
+            res += i + ", ";
+        res += "}";
+        Debug.Log(res);
+    }
+
+
+    /* CLONE METHODS */
+    public Face Clone() {
+        Face clone = Instantiate(this);
+        clone.indices = new List<int>(this.indices);
+        clone.FindSpriteRendererReferences();
+        return clone;
+    }
+
+
+    /* MEMBER HELPER METHODS */
+    protected void FindSpriteRendererReferences() {
+        head_sr = GetComponentInChildren<SpriteRenderer>();
+        ear_l_sr = head_sr.transform.Find(EarLeftName).GetComponent<SpriteRenderer>();
+        ear_r_sr = head_sr.transform.Find(EarRightName).GetComponent<SpriteRenderer>();
+        eye_l_sr = head_sr.transform.Find(EyeLeftName).GetComponent<SpriteRenderer>();
+        eye_r_sr = head_sr.transform.Find(EyeRightName).GetComponent<SpriteRenderer>();
+        nose_sr = head_sr.transform.Find(NoseName).GetComponent<SpriteRenderer>();
+    }
+
+
+    /* STATIC HELPER METHODS */
+    public static int NumFeatures () {
+        return System.Enum.GetNames(typeof(Feature)).Length;
+    }
+    public static List<Feature> BuildFeatureList () {
+        System.Array arr = System.Enum.GetValues(typeof(Feature));
+        List<Feature> list = new List<Feature>();
+        foreach (Feature f in arr) {
+            list.Add(f);
+        }
+        return list;
+    }
+}
---
+++ Assets/My Assets/Scripts/RandomFaceGenerator.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomFaceGenerator : MonoBehaviour {
+
+    /* VARIABLES AND PROPERTIES */
+    // Features Lists
+    public Face blankFace;
+    public List<GameObject> heads;
+    public List<Sprite> ears;
+    public List<Sprite> eyes;
+    public List<Sprite> noses;
+    public List<Color> skins;
+
+
+    // TESTING VARS
+    float time_per_change = 0.5f;
+    float time_since_change = 0;
+    Face cur_face;
+
+    /* LIFECYCLE METHODS */
+    private void Start () {
+        cur_face = GenerateRandomFace();
+        cur_face.DebugIndices();
+    }
+
+    private void Update () {
+        time_since_change += Time.deltaTime;
+        if(time_since_change >= time_per_change) {
+            time_since_change = 0;
+            Face next_face = GenerateRandomFaceFromParent(cur_face);
+            DestroyImmediate(cur_face.gameObject);
+            cur_face = next_face;
+            cur_face.DebugIndices();
+        }
+    }
+
+
+    /* MAIN GENERATOR FUNCTIONS */
+    // Generate a random face from scratch
+    public Face GenerateRandomFace () {
+        // Select features
+        int head_index = GetRandomIndexFromList<GameObject>(heads);
+        int skin_index = GetRandomIndexFromList<Color>(skins);
+        int ear_index = GetRandomIndexFromList<Sprite>(ears);
+        int eye_index = GetRandomIndexFromList<Sprite>(eyes);
+        int nose_index = GetRandomIndexFromList<Sprite>(noses);
+
+        // Create new head from prefab
+        Face face = Instantiate<Face>(blankFace);
+        // Fill features on face
+        face.SetHead(heads[head_index], head_index);
+        face.SetSkin(skins[skin_index], skin_index);
+        face.SetEars(ears[ear_index], ear_index);
+        face.SetEyes(eyes[eye_index], eye_index);
+        face.SetNose(noses[nose_index], nose_index);
+
+        // Return finished head
+        return face;
+    }
+
+    // Generate a random face from a parent
+    public Face GenerateRandomFaceFromParent (Face parent) {
+        int num_differences = 2; // NOTE: CAN MAKE THIS VALUE RANDOM LATER
+
+        Face face = parent.Clone();
+        List<Face.Feature> features = Face.BuildFeatureList();
+        // Loop for num_differences times to change a different facial feature
+        for(int i=0; i<num_differences; i++) {
+            Face.Feature f = GetAndRemoveRandomItemFromList<Face.Feature>(features);
+            switch (f) {
+
+                case Face.Feature.Head:
+                    int head_index = GetRandomIndexFromList<GameObject>(heads, face.GetIndexOf(Face.Feature.Head));
+                    face.SetHead(heads[head_index], head_index);
+                    break;
+
+                case Face.Feature.Skin:
+                    int skin_index = GetRandomIndexFromList<Color>(skins, face.GetIndexOf(Face.Feature.Skin));
+                    face.SetSkin(skins[skin_index], skin_index);
+                    break;
+
+                case Face.Feature.Ears:
+                    int ear_index = GetRandomIndexFromList<Sprite>(ears, face.GetIndexOf(Face.Feature.Ears));
+                    face.SetEars(ears[ear_index], ear_index);
+                    break;
+
+                case Face.Feature.Eyes:
+                    int eye_index = GetRandomIndexFromList<Sprite>(eyes, face.GetIndexOf(Face.Feature.Eyes));
+                    face.SetEyes(eyes[eye_index], eye_index);
+                    break;
+
+                case Face.Feature.Nose:
+                    int nose_index = GetRandomIndexFromList<Sprite>(noses, face.GetIndexOf(Face.Feature.Nose));
+                    face.SetNose(noses[nose_index], nose_index);
+                    break;
+
+                default:
+                    Debug.LogError("Missing case for certain Feature in GenerateRandomFaceFromParent!");
+                    break;
+            }
+        }
+        return face;
+    }
+
+
+    /* HELPER FUNCTIONS */
+    int GetRandomIndexFromList<T> (List<T> list) {
+        if (list.Count == 0)
+        {
+            Debug.LogWarning("GetRandomIndexOfList: list is empty.");
+            return -1;
+        }
+
+        int index = (int)(Random.value * list.Count);
+        if (index == list.Count)
+            index--;
+        return index;
+    }
+    int GetRandomIndexFromList<T> (List<T> list, int excluding_index) {
+        if (list.Count <= 1) {
+            Debug.LogWarning("GetRandomIndexOfList: list is too small.");
+            return -1;
+        }
+
+        int index = (int)(Random.value * (list.Count-1));
+        if (index == list.Count-1)
+            index--;
+        if (index >= excluding_index)
+            index++;
+        return index;
+    }
+    T GetRandomItemFromList<T> (List<T> list) {
+        int index = GetRandomIndexFromList<T>(list);
+        if (index < 0)
+            return default(T);
+        return list[index];
+    }
+    T GetAndRemoveRandomItemFromList<T> (List<T> list) {
+        int index = GetRandomIndexFromList<T>(list);
+        if (index < 0)
+            return default(T);
+        T item = list[index];
+        list.RemoveAt(index);
+        return item;
+    }
+}
